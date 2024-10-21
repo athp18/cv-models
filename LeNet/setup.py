ee5afa30a292df31f35b85cff4a988d4c7208525
athp18 <129908089+athp18@users.noreply.github.com>
2024-10-21 15:41:11 -0700

    update setup.py names

--- LeNet/setup.py
+++ LeNet/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='custom_conv2d',
+    name='conv2d',
     ext_modules=[
-        CUDAExtension('custom_conv2d', [
-            'custom_conv2d_kernel.cu',
+        CUDAExtension('conv2d', [
+            'conv2d_kernel.cu',
         ]),
     ],
     cmdclass={
